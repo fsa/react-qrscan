c19f01c9e59626d0472e411ec18d993f7cb7b0bc
Sergei Fedotov <fsa@tavda.info>
2025-03-26 01:16:53 +0700

    Убрано отображение истории сканирования

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import Html5QrcodePlugin from './Html5QrcodePlugin.jsx';
-import ResultContainerPlugin from './ResultContainerPlugin.jsx';
 
 const App = (props) => {
   const [decodedResults, setDecodedResults] = useState([]);
   const onNewScanResult = (decodedText, decodedResult) => {
     console.log("App [result]", decodedResult);
-    setDecodedResults(prev => [...prev, decodedResult]);
+    setDecodedResults(decodedText);
   };
 
   return (
     <div className="App">
       <section className="App-section">
-        <div className="App-section-title"> Html5-qrcode React demo</div>
+        <div className="App-section-title">Сканер QR-кодов</div>
         <br />
         <Html5QrcodePlugin
           fps={10}
@@ -23,7 +22,7 @@ const App = (props) => {
           disableFlip={false}
           qrCodeSuccessCallback={onNewScanResult}
         />
-        <ResultContainerPlugin results={decodedResults} />
+        <p>{decodedResults}</p>
       </section>
     </div>
   );
