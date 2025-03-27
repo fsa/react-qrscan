1ba57a2afc7cf7798be5df73c3893fdcd963df91
Sergei Fedotov <fsa@tavda.info>
2025-03-28 06:13:02 +0700

    Убрана работа с queryString

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,27 @@ import Html5QrcodePlugin from './Html5QrcodePlugin.jsx';
 import axios from 'axios';
 
 const App = (props) => {
-  const queryParams = new URLSearchParams(window.location.search);
-  const token = queryParams.get("token");
-  const url = queryParams.get("url");
-  const [decodedResults, setDecodedResults] = useState({data: 'Сканируйте код'});
+  const [decodedResults, setDecodedResults] = useState({ data: 'Сканируйте код' });
   const onNewScanResult = (decodedText, decodedResult) => {
     console.log("App [result]", decodedResult);
-    if (url) {
-      const headers = token ?
-        { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token } :
-        { 'Content-Type': 'application/json' };
-      axios.post(url,
-        {
-          "text": decodedText,
-          "format": decodedResult.result.format.format,
-          "format_name": decodedResult.result.format.formatName,
-        },
-        {
-          headers: headers
-        }
-      ).then((response) => {
-        setDecodedResults({data: response.data.data});
-        console.log(response);
-      })
-        .catch((error) => {
-          setDecodedResults({data: error.message});
-          console.log(error);
-        });
-    } else {
-      setDecodedResults({data: decodedResult});
+    axios.post('/scan',
+      {
+        "text": decodedText,
+        "format": decodedResult.result.format.format,
+        "format_name": decodedResult.result.format.formatName,
+      },
+      {
+        headers: { 'Content-Type': 'application/json' }
+      }
+    ).then((response) => {
+      setDecodedResults({ data: response.data.data });
+      console.log(response);
     }
+    ).catch((error) => {
+      setDecodedResults({ data: error.message });
+      console.log(error);
+    }
+    );
   };
 
   return (
