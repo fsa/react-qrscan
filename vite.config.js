aad1671960b2bd89905257bd34ed83a242f3c483
Sergei Fedotov <fsa@tavda.info>
2025-04-06 14:23:43 +0700

    Белый фон и исправлена ошибка сборки

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/scanner/'
-})
+    base: "/scanner/",
+    plugins: [
+        react()
+    ],
+    build: {
+        rollupOptions: {
+            external: ["react-dom/client"],
+        },
+    },
+});
