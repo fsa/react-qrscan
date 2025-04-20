176c3dd3733f4c6cf1a131b27787d88743862026
Sergei Fedotov <fsa@tavda.info>
2025-04-21 00:02:56 +0700

    Чистка кода и корректировка оформления

--- src/QrCodeScanner.jsx
+++ src/QrCodeScanner.jsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import './QrCodeScanner.css';
 import Html5QrCodePlugin from './components/Html5QrCodePlugin';
 import axios from 'axios';
-import { Scanner } from '@yudiel/react-qr-scanner';
 
 const QrCodeScanner = (props) => {
   const [decodedResults, setDecodedResults] = useState('Сканируйте код');
@@ -13,33 +12,6 @@ const QrCodeScanner = (props) => {
   const [codeId, setCodeId] = useState(null);
   const [editedDescription, setEditedDescription] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-
-  const onScanResult = (event) => {
-    setErrorMessage('Отправка кода на сервер...')
-    axios.post('/scan',
-      {
-        "text": event[0].rawValue,
-        "format": 0,
-        "format_name": event[0].format,
-      },
-      {
-        headers: { 'Content-Type': 'application/json' }
-      }
-    ).then((response) => {
-      setErrorMessage('');
-      setCodeId(response.data.id);
-      setDecodedResults(response.data.data);
-      setEditable(response.data.editable);
-      setEditedDescription(response.data.description || '');
-      setEditMode(false);
-    }
-    ).catch((error) => {
-      setErrorMessage(error.message);
-      setEditable(false);
-      setEditMode(false);
-    }
-    );
-  }
 
   const onNewScanResult = (decodedText, decodedResult) => {
     setErrorMessage('Отправка кода на сервер...')
@@ -101,7 +73,6 @@ const QrCodeScanner = (props) => {
 
   return (
     <div className="App">
-      <div>{errorMessage}</div>
       <section className="App-section">
         <Html5QrCodePlugin
           fps={10}
@@ -110,6 +81,7 @@ const QrCodeScanner = (props) => {
           qrCodeSuccessCallback={onNewScanResult}
         />
       </section>
+      <div>{errorMessage}</div>
       {editMode ? (
         <div className="edit-form">
           <p>{decodedResults}</p>
